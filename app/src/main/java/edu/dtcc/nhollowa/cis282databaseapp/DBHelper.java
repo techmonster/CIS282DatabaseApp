03b1a802c57e19c196f9b0d2de15c2db2fbef24c
techmonster <n8talent@gmail.com>
2016-04-27 11:36:39 -0400

    2nd version

--- app/src/main/java/edu/dtcc/nhollowa/cis282databaseapp/DBHelper.java
+++ app/src/main/java/edu/dtcc/nhollowa/cis282databaseapp/DBHelper.java
@@ -1,12 +1,15 @@
 package edu.dtcc.nhollowa.cis282databaseapp;
 
 import android.os.AsyncTask;
+import android.widget.TextView;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 
 /**
@@ -56,21 +59,49 @@ public class DBHelper {
                 String sql4 = "SELECT * FROM TASK_EMPLOYEE";
 
 
+                String result = "";
 
+                ArrayList<String> returnedData = new ArrayList<>();
                 final ResultSet rs;
                 if (st != null) {
-                    rs = st.executeQuery(sql1);
+                    rs = st.executeQuery(sql2);
 
-                    while (rs.next()) {
-                        System.out.println(rs.getString(1) + "\t" + rs.getString(2)+ "\t" +rs.getString(3)
-                                + "\t" +rs.getString(4));
+                    returnedData = populateData(rs);
+
+                    for (String s : returnedData) {
+                        result += s;
                     }
+
+
                 }
+                System.out.print(result);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
 
             return null;
+        }
+
+        public ArrayList<String> populateData(ResultSet set){
+
+            String result;
+            ArrayList<String> data = new ArrayList<>();
+            try{
+                while (set.next()) {
+                    result = null;
+                    result += set.getInt(1) + "\t";
+                    //result += rsmd.getColumnName(2) + ":" + set.getInt(2) + "\n";
+                    result += set.getString(2) + "\t";
+                    result += set.getString(3) + "\n";
+
+                    data.add(result);
+                }
+
+            } catch (Exception e){
+                System.out.println(e);
+            }
+            return data;
+
         }
     }
 
